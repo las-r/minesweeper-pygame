6d9f720965301cc8e9d23c445afc23152729625a
las_r <128936184+las-r@users.noreply.github.com>
2025-03-03 14:51:42 -0500

    Add details to executable

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from cx_Freeze import setup, Executable
 
 setup(
-    name="minesweeper",
+    name="Minesweeper",
+    version="1.1.1",
+    description="Minesweeper written in Pygame.",
     options={
         "build_exe": {
             "packages": ["pygame"],
-            "include_files": ["sprites/", "sounds/"]
+            "include_files": ["sprites/", "sounds/"],
         }
     },
     executables=[Executable("minesweeper.py", base="Win32GUI", icon="icon.ico")]
